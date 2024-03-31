39d7f4f36cc6592249775b13f2ff21fa7e6e96fe
satoki252595 <48168408+satoki252595@users.noreply.github.com>
2024-04-01 08:45:12 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -12,18 +12,18 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Youtube`,
+      href: "https://www.youtube.com/channel/UC1gGJzc9nPDlV_GZMktA-8A",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "はぴまねの株ラボ",
     description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://happy-money-kabu-labo.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "ja-JP", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
